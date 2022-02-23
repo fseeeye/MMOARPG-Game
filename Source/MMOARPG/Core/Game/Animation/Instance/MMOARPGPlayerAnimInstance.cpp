b848425e3498e82fa80f5247bb9c71f55648580b
fseeeye <fseeeye@gmail.com>
2022-02-24 00:52:56 +0800

    feat(game): impl KeadingFace data to Player AnimInstance

--- Source/MMOARPG/Core/Game/Animation/Instance/MMOARPGPlayerAnimInstance.cpp
+++ Source/MMOARPG/Core/Game/Animation/Instance/MMOARPGPlayerAnimInstance.cpp
@@ -2,3 +2,20 @@
 
 
 #include "MMOARPGPlayerAnimInstance.h"
+#include "../../../Common/Interface/KneadingInterface.h"
+
+void UMMOARPGPlayerAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	// Update KeadingFace data
+	if (IKneadingInterface* KneadingInterface = Cast<IKneadingInterface>(TryGetPawnOwner()))
+	{
+		LegSize = KneadingInterface->GetLegSize();
+		WaistSize = KneadingInterface->GetWaistSize();
+		ArmSize = KneadingInterface->GetArmSize();
+		// ...
+	}
+
+
+}
--- Source/MMOARPG/Core/Game/Animation/Instance/MMOARPGPlayerAnimInstance.h
+++ Source/MMOARPG/Core/Game/Animation/Instance/MMOARPGPlayerAnimInstance.h
@@ -13,5 +13,18 @@ UCLASS()
 class MMOARPG_API UMMOARPGPlayerAnimInstance : public UMMOARPGCharacterAnimInstance
 {
 	GENERATED_BODY()
+
+public:
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
 	
+	/*** KneadingFace ***/
+public:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AnimAttributes")
+	float LegSize;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AnimAttributes")
+	float WaistSize;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AnimAttributes")
+	float ArmSize;
 };
