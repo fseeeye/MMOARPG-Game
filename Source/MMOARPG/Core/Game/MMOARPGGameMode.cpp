67412c304d64b95866a08dcbace370cfcc52dbb9
fseeeye <fseeeye@gmail.com>
2022-03-08 01:26:54 +0800

    feat(fly): init Fly component & rename some files

--- Source/MMOARPG/Core/Game/MMOARPGGameMode.cpp
+++ Source/MMOARPG/Core/Game/MMOARPGGameMode.cpp
@@ -32,7 +32,7 @@ AMMOARPGGameMode::AMMOARPGGameMode()
 	PlayerControllerClass = AMMOARPGPlayerController::StaticClass();
 
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Characters/Blueprints/BP_Player"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
