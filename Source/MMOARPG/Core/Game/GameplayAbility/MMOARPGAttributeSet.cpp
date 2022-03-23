f7ceeba886c4ea432febe5415cc08dcccf9eaa1c
fseeeye <fseeeye@gmail.com>
2022-03-24 00:15:30 +0800

    feat(gas): add attributes to attribute set

--- Source/MMOARPG/Core/Game/GameplayAbility/MMOARPGAttributeSet.cpp
+++ Source/MMOARPG/Core/Game/GameplayAbility/MMOARPGAttributeSet.cpp
@@ -1,9 +1,25 @@
 #include "MMOARPGAttributeSet.h"
 
+#include <Net/UnrealNetwork.h>
 
 UMMOARPGAttributeSet::UMMOARPGAttributeSet()
+	: Health(100.f)
+	, MaxHealth(100.f)
+	, Mana(50.f)
+	, MaxMana(50.f)
 {
 
+}
+
+void UMMOARPGAttributeSet::GetLifetimeReplicatedProps(TArray< class FLifetimeProperty >& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	// replicated properties
+	DOREPLIFETIME(UMMOARPGAttributeSet, Health);
+	DOREPLIFETIME(UMMOARPGAttributeSet, MaxHealth);
+	DOREPLIFETIME(UMMOARPGAttributeSet, Mana);
+	DOREPLIFETIME(UMMOARPGAttributeSet, MaxMana);
 }
 
 void UMMOARPGAttributeSet::PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data)
@@ -12,4 +28,24 @@ void UMMOARPGAttributeSet::PostGameplayEffectExecute(const struct FGameplayEffec
 
 	// TODO: Calc to modify attributes.
 
+}
+
+void UMMOARPGAttributeSet::OnRep_Health(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UMMOARPGAttributeSet, Health, OldValue);
+}
+
+void UMMOARPGAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UMMOARPGAttributeSet, MaxHealth, OldValue);
+}
+
+void UMMOARPGAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UMMOARPGAttributeSet, Mana, OldValue);
+}
+
+void UMMOARPGAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UMMOARPGAttributeSet, MaxMana, OldValue);
 }
--- Source/MMOARPG/Core/Game/GameplayAbility/MMOARPGAttributeSet.h
+++ Source/MMOARPG/Core/Game/GameplayAbility/MMOARPGAttributeSet.h
@@ -2,6 +2,9 @@
 
 #include <AttributeSet.h>
 
+#include "MMOARPGGameplayAbilityMacro.h"
+#include <AbilitySystemComponent.h>
+
 #include "MMOARPGAttributeSet.generated.h"
 
 UCLASS()
@@ -12,9 +15,42 @@ class MMOARPG_API UMMOARPGAttributeSet : public UAttributeSet
 public:
 	UMMOARPGAttributeSet();
 
+	/** Returns properties that are replicated for the lifetime of the actor channel */
+	virtual void GetLifetimeReplicatedProps(TArray< class FLifetimeProperty >& OutLifetimeProps) const;
+
 	/**
 	 *	Called just before a GameplayEffect is executed to modify the base value of an attribute. No more changes can be made.
 	 *	Note this is only called during an 'execute'. E.g., a modification to the 'base value' of an attribute. It is not called during an application of a GameplayEffect, such as a 5 ssecond +10 movement speed buff.
 	 */
 	virtual void PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data) override;
+
+protected:
+	UFUNCTION()
+	virtual void OnRep_Health(const FGameplayAttributeData& OldValue);
+
+	UFUNCTION()
+	virtual void OnRep_MaxHealth(const FGameplayAttributeData& OldValue);
+
+	UFUNCTION()
+	virtual void OnRep_Mana(const FGameplayAttributeData& OldValue);
+
+	UFUNCTION()
+	virtual void OnRep_MaxMana(const FGameplayAttributeData& OldValue);
+
+public:
+	UPROPERTY(BlueprintReadOnly, Category = "Health", ReplicatedUsing = OnRep_Health)
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UMMOARPGAttributeSet, Health)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Health", ReplicatedUsing = OnRep_MaxHealth)
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UMMOARPGAttributeSet, MaxHealth)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Mana", ReplicatedUsing = OnRep_Mana)
+	FGameplayAttributeData Mana;
+	ATTRIBUTE_ACCESSORS(UMMOARPGAttributeSet, Mana)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Mana", ReplicatedUsing = OnRep_MaxMana)
+	FGameplayAttributeData MaxMana;
+	ATTRIBUTE_ACCESSORS(UMMOARPGAttributeSet, MaxMana)
 };
